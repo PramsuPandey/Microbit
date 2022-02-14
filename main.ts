e5d095b5f46ba5b0429a66255f454b8668046e65
Pramsu Pandey <83841968+PramsuPandey@users.noreply.github.com>
2022-02-14 09:38:17 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 1e+152; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart)
+    }
+})
